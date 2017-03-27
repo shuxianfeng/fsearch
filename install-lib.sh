bb9ab357c175541fed2cf6966f432df3b8cdf25e
ywgitlabadmin <zhuangyuhao711@163.com>
2017-03-27 20:36:58 +0800

    1

--- install-lib.sh
+++ install-lib.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 #prepare
 projectDir=$(cd "$(dirname "$0")"; pwd)
+code_path="/home/app/projects/fsearch"
+version_name=$1
+
+cd "$code_path"
+git checkout master
+git pull
+
+if [ -n $version_name ];
+then
+  git checkout $version_name
+  git pull
+fi
 
 mvn install:install-file -Dfile="$projectDir/src/main/lib/petkit-base.jar" -DgroupId=com.petkit -DartifactId=petkit-base -Dversion=4.0 -Dpackaging=jar
--- start.sh
+++ start.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
 #prepare
-code_path="/home/app/projects/fsearch"
-version_name=$1
 baseDir=$(cd "$(dirname "$0")"; pwd)
 className=Main
-
-cd "$code_path"
-git checkout master
-git pull
-
-if [ -n $version_name ];
-then
-  git checkout $version_name
-  git pull
-fi
 
 java -cp "$baseDir/target/lib/*" $className &
