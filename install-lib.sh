93ba68404d9db0cb6859055042c0990d74cb61f6
ywgitlabadmin <zhuangyuhao711@163.com>
2017-03-27 19:41:40 +0800

    1

--- install-lib.sh
+++ install-lib.sh
@@ -1,2 +1,12 @@
+#!/bin/bash
+#prepare
+version_name=$1
 projectDir=$(cd "$(dirname "$0")"; pwd)
-mvn install:install-file -Dfile="$projectDir/src/main/lib/petkit-base.jar" -DgroupId=com.petkit -DartifactId=zhb-petkit-base -Dversion=4.0 -Dpackaging=jar
+
+if [ -n $version_name ];
+then
+  git checkout $version_name
+  git pull
+fi
+
+mvn install:install-file -Dfile="$projectDir/src/main/lib/petkit-base.jar" -DgroupId=com.petkit -DartifactId=petkit-base -Dversion=4.0 -Dpackaging=jar
