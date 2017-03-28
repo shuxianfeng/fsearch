6e9d089c0a6130cae3941e950f3d9842b786475d
ywgitlabadmin <zhuangyuhao711@163.com>
2017-03-28 13:09:18 +0800

    1

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #prepare
-baseDir=$(cd "$(dirname "$0")"; pwd)
+#baseDir=$(cd "$(dirname "$0")"; pwd)
+baseDir=/home/app/projects/fsearch
 className=Main
 
 java -cp "$baseDir/target/lib/*" $className &
