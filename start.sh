0fe291a62360be1d9fa8079e8203fedfe31b45c1
ywgitlabadmin <zhuangyuhao711@163.com>
2017-03-28 13:20:51 +0800

    1

--- start.sh
+++ start.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 #prepare
 #baseDir=$(cd "$(dirname "$0")"; pwd)
-baseDir=/home/app/projects/fsearch
-className=com.movision.fsearch.Main
+#baseDir=/home/app/projects/fsearch
+#className=com.movision.fsearch.Main
+#java -cp "$baseDir/target/lib/*" $className &
 
-java -cp "$baseDir/target/lib/*" $className &
+#Java程序所在的目录
+APP_HOME=/home/app/projects/fsearch
+
+#需要启动的Java主程序（main方法类）
+APP_MAINCLASS=com.movision.fsearch.Main
+
+#拼凑完整的classpath参数，包括指定lib目录下所有的jar
+CLASSPATH=$APP_HOME/classes
+for i in "$APP_HOME"/lib/*.jar; do
+   CLASSPATH="$CLASSPATH":"$i"
+done
+
+java -classpath $CLASSPATH $APP_MAINCLASS &
+
